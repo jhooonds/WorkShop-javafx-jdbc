0d6435d606a367448c27b5ca73bb964b63f9651d
Jonatan Silva <41707797+jhooonds@users.noreply.github.com>
2019-12-31 01:01:03 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
@@ -18,12 +21,15 @@ public class Main extends Application {
 			scrollPane.setFitToWidth(true);
 			
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 }
