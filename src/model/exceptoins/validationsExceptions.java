8f36e659b8bfd1132f9e51122356d9489ffaf7a3
Jonatan Silva <41707797+jhooonds@users.noreply.github.com>
2020-01-04 00:15:04 -0300

    Validation Exceptions

---
+++ src/model/exceptoins/validationsExceptions.java
@@ -0,0 +1,24 @@
+package model.exceptoins;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class validationsExceptions extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Map<String, String> errors = new HashMap<>();
+	
+	public validationsExceptions(String msg) {
+		super(msg);
+	}
+	
+	public Map<String, String> getErrors(){
+		return errors;
+	}
+	
+	public void addError(String fieldName, String errorMessage) {
+		errors.put(fieldName, errorMessage);
+	}
+
+}
